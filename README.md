f95530febe6c4f822e4abc83e4a0a159216faf4c
Rusty Bower <rusty@rustybower.com>
2018-07-13 14:49:31 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build Status](https://travis-ci.org/RustyBower/sopel-urban.svg?branch=master)](https://travis-ci.org/RustyBower/sopel-bucket)
+[![Maintainability](https://api.codeclimate.com/v1/badges/43154ee379640b3e56a0/maintainability)](https://codeclimate.com/github/RustyBower/sopel-urban/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/43154ee379640b3e56a0/test_coverage)](https://codeclimate.com/github/RustyBower/sopel-urban/test_coverage)
+
+# sopel-urban
+
+sopel-urban is an Urban Dictionary module for Sopel
---
+++ sopel_modules/urban/urban.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+# Copyright 2018 Rusty Bower
+# Licensed under the Eiffel Forum License 2
+import requests
+
+from sopel.module import commands, example, NOLIMIT
+
+
+def display(bot, term, data):
+    definition = data['list'][0]['definition']
+    bot.say('{term} - {definition})'.format(term=term, definition=definition))
+
+
+def get_definition(bot, term):
+    data = None
+    try:
+        data = requests.get('https://api.urbandictionary.com/v0/define?term={term}'.format(term=term)).json()[0]
+        return data
+    except Exception:
+        raise
+
+
+@commands('ud', 'urban')
+@example('.urban the rusty bower')
+def urban(bot, trigger):
+    term = trigger.group(2)
+    # Get data from API
+    data = get_definition(bot, term)
+    # Have the bot print the data
+    if data:
+        if data['list'] > 0:
+            display(bot, term, data)
+
